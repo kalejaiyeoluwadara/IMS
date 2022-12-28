87d6b510feced53ae6c7ca9bf7e62be8209f07c4
kalejaiyeoluwadara <90782485+kalejaiyeoluwadara@users.noreply.github.com>
2022-12-28 21:02:38 +0100

    Add files via upload

--- IMS/myg.js
+++ IMS/myg.js
@@ -1,21 +1,14 @@
-
+//Function for Enter button.
 function enterb(){
     var uname = (document.getElementById("name").value).toLowerCase();
     var arr = (uname[0]);
     var arr2 = (uname.substr(0,2));
-    console.log(arr2);
     let gpa = ['a','b'];
     let gpb = ['l','m','n','o'];
     let gpc = ['c','d','e','f','g','h','i','j','k'];
     let gpd = ['o','p','e','r','s','t','u','v','w','x','y','z'];
     let allgp = gpa.concat(gpb).concat(gpc).concat(gpd);
 
-    // if(arr2 == 'ok'){
-    //     console.log("Group B");
-    // }
-    // else if(arr2 == 'ol'){
-    //     console.log("Group D");
-    // }
     for(let i = 0;i<allgp.length;i++){
         if(arr == gpa[i]){
             shout('a');
@@ -32,13 +25,10 @@ function enterb(){
        
       
     }
-
-
-
-
-    
-
 }
+
+
+//Menu Animation 
 function shout(msg){
     let msgbox = document.getElementById('msg');
     msgbox.innerHTML = "You're in group " + msg.toUpperCase();
@@ -50,20 +40,12 @@ function nav(){
         document.getElementById('main').style.height = '300px';
         document.getElementById('links').style.visibility = 'visible';
         document.getElementById('ul').style.transform = "translateX(0%)";
-        // for(let i = 0; i<lists.length;i++){
-        //     lists[i].style.transform = "translateX(0%)";
-        //     console.log(lists[i]);
-        // }
         bool = false;
     }
     else if(bool == false){
         document.getElementById('main').style.height = '90px';
         document.getElementById('links').style.visibility = 'hidden';
         document.getElementById('ul').style.transform = "translateX(-100%)";
-        // for(let i = 0; i<lists.length;i++){
-        //     lists[i].style.transform = "translateX(-100%)";
-        //     console.log(lists[i]);
-        // }
         bool = true;
     }
     else{
---
+++ IMS/timetab.js
@@ -0,0 +1,14 @@
+const http = require('fs');
+// const port = 3000;
+// const server = http.createServer((req,res)=>{
+
+// });
+// server.listen(port,(err)=>{
+//     if(err){
+//         console.log("Something went wrong" + err);
+//     }
+//     else{
+//         console.log("server is listening on port "+ port);
+//     }
+// })
+console.log(http);
